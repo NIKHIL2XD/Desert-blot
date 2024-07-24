e7502dd2dfe5a26d93e7e39f2ebc3df8cebc26ee
Nikhil Mansotra <120664903+NIKHIL2XD@users.noreply.github.com>
2024-07-25 00:48:26 +0530

    Create feather.js

---
+++ feather/feather.js
@@ -0,0 +1,28 @@
+// check out the workshop tab to get started
+// welcome to blot!
+
+// check out this guide to learn how to program in blot
+// https://blot.hackclub.com/editor?guide=start
+
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+
+// store final lines here
+const finalLines = [];
+
+// code
+const shth = 3
+const shl = 120 
+const cl = 30  
+const vw = 20  
+const t = new bt.Turtle()
+
+
+
+// transform lines using the toolkit
+bt.rotate(finalLines, 45);
+
+// draw it
+drawLines(finalLines);
