fe09218414fc2aafa3f58b9631e5a98d04c76611
Nikhil Mansotra <120664903+NIKHIL2XD@users.noreply.github.com>
2024-07-27 18:56:37 +0530

    Update desert.js

--- modern city/city.js
+++ modern city/city.js
@@ -1,56 +1,33 @@
-/*
-there are three built in names
+//@title: Modern City
+//@author: Nikhil
+//@snapshot: desert.png
 
-setDocDimensions
-drawLines
-blotToolkit (also called bt)
-
-the toolkit has lots of useful functions we can access
-*/
-
-const width = 200;
-const height = 120;
+const width = 180;
+const height = 115;
 
 setDocDimensions(width, height);
+const turtle = new bt.Turtle()
 
-const finalLines = []; // we will put our shapes here
-bt.join(finalLines, rect(159, 100)); // here is the new line
-bt.join(finalLines, rect(67, 52)); 
-function rect(w, h) {
 
-  // notice how this is an array of arrays
-  return [
-    [
-      [-w/2, h/2],
-      [w/2, h/2],
-      [w/2, -h/2],
-      [-w/2, -h/2],
-      [-w/2, h/2],
-    ]
+//constants
+const sunRad = 10;
+const sunRays = 20;
+
+//bottom line
+drawLines([
+  [
+    [0, 40],
+    [180, 40]
   ]
+]);
+
+
+//sun 
+for (let i = 1; i < sunRays + 63; i++) {
+  let angle = -i / (sunRays + 1) * 92;
+  let distance = i % -2 == 1 ? 14 : 9;
+  turtle.jump([sunRad, height]).setAngle(260).down().arc(angle, sunRad); // go to pos
+  turtle.setAngle(angle).up().forward(0.2).down().forward(distance); // sun ray
 }
 
-const gridWidth = 10
-const squareWidth = 10
-const squareHeight = 10
-
-for (let i = 0; i < 2; i++) {
-  for (let j = 0; j < 2; j++) {
-    const square = rect(squareWidth, squareHeight)
-    bt.translate(square, [squareWidth * i, squareHeight * j])
-    bt.join(finalLines, square);
-  }
-}
-
-
-// let's get the bounds of our final lines
-const finalLinesBounds = bt.bounds(finalLines);
-
-// this moves the center of our drawing to the center of our doc
-bt.translate(
-  finalLines,
-  [ width / 2, height / 2 ], 
-  finalLinesBounds.cc
-); 
-
-drawLines(finalLines);
+drawLines(turtle.lines());
