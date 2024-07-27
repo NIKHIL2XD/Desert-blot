5b82cc064afcba8a7ebc3b8f11c8b439a9f480ba
Nikhil Mansotra <120664903+NIKHIL2XD@users.noreply.github.com>
2024-07-27 23:07:18 +0530

    Update city.js

--- modern city/city.js
+++ modern city/city.js
@@ -10,8 +10,10 @@ const turtle = new bt.Turtle()
 
 
 //constants
-const sunRad = 10;
+const sunRad = 9;
 const sunRays = 20;
+const random = bt.randIntInRange(-1.0, 4)
+const randomrays = bt.randIntInRange(9, 20)
 
 //bottom line
 drawLines([
@@ -21,13 +23,21 @@ drawLines([
   ]
 ]);
 
-
-//sun 
-for (let i = 1; i < sunRays + 63; i++) {
-  let angle = -i / (sunRays + 1) * 92;
-  let distance = i % -2 == 1 ? 14 : 9;
-  turtle.jump([sunRad, height]).setAngle(260).down().arc(angle, sunRad); // go to pos
+//sun
+turtle.jump([sunRad, height]).setAngle(270).down().arc(-90, sunRad); // circle
+for (let i = 1; i < sunRays + 65; i++) {
+  let angle = -i / (sunRays + 1) * 90;
+  let distance = i % 2 == 1 ? randomrays : 6;
+  turtle.jump([sunRad, height]).setAngle(270).down().arc(angle, sunRad); // go to pos
   turtle.setAngle(angle).up().forward(0.2).down().forward(distance); // sun ray
 }
 
-drawLines(turtle.lines());
+function centerPolylines(polylines, documentWidth, documentHeight) {
+  const cc = bt.bounds(polylines).cc;
+  bt.translate(polylines, [documentWidth / 12.0, documentHeight / 1.2], cc);
+}
+
+let sun = turtle.lines()
+centerPolylines(sun, width, height)
+
+drawLines(sun);
