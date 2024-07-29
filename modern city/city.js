f4bb9483762107c8a480cf8494b8f80d06d267c8
Nikhil Mansotra <120664903+NIKHIL2XD@users.noreply.github.com>
2024-07-29 10:21:21 +0530

    Update city.js

--- modern city/city.js
+++ modern city/city.js
@@ -67,18 +67,23 @@ for (let i = 1; i < sunRays + 65; i++) {
   turtle.setAngle(angle).up().forward(0.2).down().forward(distance); // sun ray
 }
 
-function centerPolylines(polylines, documentWidth, documentHeight) {
-  const cc = bt.bounds(polylines).cc;
-  bt.translate(polylines, [documentWidth / 12.0, documentHeight / 1.2], cc);
-}
+
 
 // random cactus position
 //cactus1
-const r1posx = bt.randIntInRange(5.0, 17)
+const r1posx = bt.randIntInRange(4.0, 17)
 const r1posy = bt.randIntInRange(2.3, 4.8)
+//cactus2
+const r2posx = bt.randIntInRange(3.0, 1.5)
+const r2posy = bt.randIntInRange(2.3, 4.8)
+//cactus3
+const r3posx = bt.randIntInRange(1.4, 1.1)
+const r3posy = bt.randIntInRange(2.3, 4.8)
 
 
 // random cactus shape
+
+//cactus1
 //right
 const rc1 = bt.randIntInRange(13, 21)
 const rc2 = bt.randIntInRange(35, 45)
@@ -89,11 +94,39 @@ const rc4 = bt.randIntInRange(50, 70)
 const rc6 = bt.randIntInRange(20, 33)
 const rc7 = bt.randIntInRange(37, 62)
 const rc10 = bt.randIntInRange(0, -3)
-
 //bottom
 const rc8 = bt.randIntInRange(15, 25)
 const rc9 = bt.randIntInRange(9, 14)
 
+//cactus2
+//right
+const rc11 = bt.randIntInRange(13, 21)
+const rc12 = bt.randIntInRange(35, 45)
+//mid
+const rc13 = bt.randIntInRange(41, 49)
+const rc14 = bt.randIntInRange(50, 70)
+//left
+const rc16 = bt.randIntInRange(20, 33)
+const rc17 = bt.randIntInRange(37, 62)
+const rc110 = bt.randIntInRange(0, -3)
+//bottom
+const rc18 = bt.randIntInRange(15, 25)
+const rc19 = bt.randIntInRange(9, 14)
+
+//cactus3
+//right
+const rc21 = bt.randIntInRange(13, 21)
+const rc22 = bt.randIntInRange(35, 45)
+//mid
+const rc23 = bt.randIntInRange(51, 65)
+const rc24 = bt.randIntInRange(60, 80)
+//left
+const rc26 = bt.randIntInRange(20, 33)
+const rc27 = bt.randIntInRange(37, 62)
+const rc210 = bt.randIntInRange(0, -3)
+//bottom
+const rc28 = bt.randIntInRange(15, 25)
+const rc29 = bt.randIntInRange(9, 14)
 
 
 //cactus
@@ -122,20 +155,87 @@ const cactus1 = [
     [rc9, rc8]  ])
 ];
 
+const cactus2 = [
+  bt.nurbs([
+    [9, rc18],
+    [7, 25],
+    [6, rc16],
+    [rc110, 35],
+    [rc110, rc17],
+    [5, 35],
+    [7, 33],
+    [5, rc13],
+    [10, rc14],
+    [16, rc13],
+    [13, 33],
+    [15, 29],
+    [16, 33],
+    [15, 36],
+    [21, rc12],
+    [20, 33],
+    [20, 31],
+    [rc11, 25],
+    [11, 21],
+    [rc19, rc18]  ])
+];
+
+const cactus3 = [
+  bt.nurbs([
+    [9, rc28],
+    [7, 25],
+    [6, rc26],
+    [rc210, 35],
+    [rc210, rc27],
+    [5, 35],
+    [7, 33],
+    [5, rc23],
+    [10, rc24],
+    [16, rc23],
+    [13, 33],
+    [15, 29],
+    [16, 33],
+    [15, 36],
+    [21, rc22],
+    [20, 33],
+    [20, 31],
+    [rc21, 25],
+    [11, 21],
+    [rc29, rc28]  ])
+];
+
 
 //centering
-function centerCactus(cactus1, documentWidth, documentHeight) {
+function centerPolylines(polylines, documentWidth, documentHeight) {
+  const cc = bt.bounds(polylines).cc;
+  bt.translate(polylines, [documentWidth / 12.0, documentHeight / 1.2], cc);
+};
+
+function centerCactus1(cactus1, documentWidth, documentHeight) {
   const cc = bt.bounds(cactus1).cc;
   bt.translate(cactus1, [documentWidth / r1posx, documentHeight / r1posy], cc);
-}
+};
+
+function centerCactus2(cactus2, documentWidth, documentHeight) {
+  const cc = bt.bounds(cactus2).cc;
+  bt.translate(cactus2, [documentWidth / r2posx, documentHeight / r2posy], cc);
+};
+
+function centerCactus3(cactus3, documentWidth, documentHeight) {
+  const cc = bt.bounds(cactus3).cc;
+  bt.translate(cactus3, [documentWidth / r3posx, documentHeight / r3posy], cc);
+};
 
 
 
 //printing
 
 let sun = turtle.lines()
-centerCactus(cactus1, width, height)
+centerCactus1(cactus1, width, height)
+centerCactus2(cactus2, width, height)
+centerCactus3(cactus3, width, height)
 centerPolylines(sun, width, height)
 drawLines(sun);
 drawLines(finalLines);
 drawLines(cactus1);
+drawLines(cactus2);
+drawLines(cactus3);
